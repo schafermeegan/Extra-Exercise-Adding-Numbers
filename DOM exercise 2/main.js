36abe92f162ed0ecbf4032e66a455f0634e7ea37
Meegan Schafer <45988365+schafermeegan@users.noreply.github.com>
2019-02-28 19:15:58 -0500

    Add files via upload

---
+++ DOM exercise 2/main.js
@@ -0,0 +1,9 @@
+function addNumbers(){
+
+let y=parseInt(document.getElementById("number1").value);
+
+let x=parseInt(document.getElementById("number2").value);
+
+let z = y+x;
+document.getElementById("addme").innerHTML = z;
+}
